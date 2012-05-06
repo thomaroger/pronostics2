baa5f759044faf72ecf85b566c9be4b0640a0728
Thomas ROGER <thomaroger@gmail.com>
2012-05-06 23:50:40 +0200

    add championship list

--- application/models/modelchampionship.php
+++ application/models/modelchampionship.php
@@ -1,12 +1,20 @@
 <?php
 class Modelchampionship extends CI_MODEL {
 
+  const WEEK = 1209600;// 14 days;
+  
   public function getChampionships($user){
     
     $this->db->from('Championship');
     $this->db->join('Championship_has_User', ' Championship_has_User.Championship_Id = Championship.Championship_Id');
     $this->db->join('GameType', ' GameType.GameType_Id = Championship.GameType_Id');
-    $this->db->where('Championship_has_User.User_Id', $user->User_Id); 
+    $this->db->join('Day', ' Day.Championship_Id = Championship.Championship_Id', 'left');
+    
+    $where = array('Championship_has_User.User_Id' => $user->User_Id,
+                   'Day.Day_Prognosis_Begin >' => time() - self::WEEK,
+                   'Day.Day_Prognosis_End < '=> time() + self::WEEK);
+    
+    $this->db->where($where); 
     $query = $this->db->get();
     return $query->result();
   }
