c5ea3f1177111857949fb12b16bf5b5917d56883
Thomas ROGER <thomaroger@gmail.com>
2012-05-06 11:59:34 +0200

    check signin

--- application/views/headerTemplate.php
+++ application/views/headerTemplate.php
@@ -5,11 +5,13 @@
     <title>Pronostics</title>
 
 	<link rel="stylesheet" type="text/css" href="/css/pronostics.css" />
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
 
 	<script type="text/javascript" src="/js/jquery.js"></script>
 	<script type="text/javascript" src="/js/jquery-ui.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.js"></script>
 	<script type="text/javascript" src="/js/pronostics.js"></script>
 
 </head>
 <body>
-<div id="pronostics">
+<div class="container">
